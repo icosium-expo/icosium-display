f4e582025088a2e5399d65e9d5405a71e961a62e
Icosium Display <contact@icosium-expo.com>
2026-09-22 14:26:49 -0700

    Mise a jour automatique - 22/09/2026 14:26

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header({ mobileMenuOpen, setMobileMenuOpen }) {
             )
         },
         {
-            href: '#salons',
+            href: '#salons et foires',
             label: 'Salons et Foires',
             icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
         },
